49b3b766b05ba605b423d2a88dbbf73bf79bb7fc
Pablo Chavez <plchavez@usf.edu>
2025-01-30 16:17:43 -0500

    Complete Task 2

---
+++ README.md
@@ -0,0 +1,4 @@
+# IN class assignment 1
+
+Follow along coding with Dr. Reed, like a child.
+
---
+++ in_class_assignment_1.js
@@ -0,0 +1,23 @@
+// Task 1
+
+let x =10;
+if (x > 5) {
+    console.log("Help");
+};
+
+let expenses = 10000;
+
+if (expenses > 7000) {
+console.log("High Expenses") ;   
+} else {
+    console.log("Managaeable Expenses");
+};
+
+// Task 2
+let score = 75;
+if (score >= 60) {
+    console.log("Passed");
+} else {
+    console.log("Failed");
+};
+
